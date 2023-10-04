c305ce1986dc57d4880a3631158671bcf8514803
DylanPiriou <dylan.piriou@hotmail.com>
2023-10-04 15:45:57 +0200

    feat(home): aria-label & alt added

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -14,16 +14,23 @@ async function getPhotographers() {
         photographers.forEach((photographer) => {
             // Création de l'élément article qui contient les infos
             const photographerArticle = document.createElement("article");
+            photographerArticle.id = photographer.id;
+            photographerArticle.setAttribute("aria-label", `Profil de ${photographer.name}`)
 
             // Création des différents éléments qui composent l'article
             const photographerImg = document.createElement("img");
             photographerImg.src = `./assets/photographers/${photographer.portrait}`;
+            photographerImg.alt = photographer.name;
+
             const photographerName = document.createElement("h2");
             photographerName.textContent = photographer.name;
+
             const photographerCity = document.createElement("span");
             photographerCity.textContent = photographer.city + ", " + photographer.country;
+
             const photographerTagline = document.createElement("p");
             photographerTagline.textContent = photographer.tagline;
+
             const photographerPrice = document.createElement("small");
             photographerPrice.textContent = photographer.price + "€/jour"
 
