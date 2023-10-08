2c9f3c41756425f5fce60e33c3f9e2c9a832d5b6
DylanPiriou <dylan.piriou@hotmail.com>
2023-10-08 15:07:42 +0200

    img grid improved

--- scripts/components/photographerGallery.js
+++ scripts/components/photographerGallery.js
@@ -1,15 +1,36 @@
 export function createGallery(id, filteredMedia) {
     const grid = document.querySelector(".gallery_grid");
-    
+
     // Parcourir chaque élément filtré
     filteredMedia.forEach(item => {
+        // Créer les nouveaux éléments du DOM
+        const imgWrapper = document.createElement("div");
+        imgWrapper.className = "gallery_img_wrapper";
+        const imgContent = document.createElement("div");
+        imgContent.className = "gallery_img_content";
+        const imgTitle = document.createElement("h3");
+        imgTitle.className = "gallery_img_title"
+        const likesWrapper = document.createElement("span");
+        likesWrapper.className = "gallery_img_likes_wrapper";
+        const likesNumber = document.createElement("p");
+        const likesLogo = document.createElement("img");
+        likesLogo.src = "../../assets/icons/heart-red.svg";
+
         // Créer une balise <img> pour chaque élément image
         if (item.image) {
             const img = document.createElement("img");
             img.src = `./assets/gallery/${id}/${item.image}`;
             img.alt = item.title;
             img.setAttribute('aria-label', `voir l'image '${item.title}'`)
-            grid.appendChild(img);
+            imgWrapper.appendChild(img);
+            imgTitle.textContent = item.title;
+            likesNumber.textContent = item.likes;
+            likesWrapper.appendChild(likesNumber);
+            likesWrapper.appendChild(likesLogo);
+            imgContent.appendChild(imgTitle);
+            imgContent.appendChild(likesWrapper);
+            imgWrapper.appendChild(imgContent);
+            grid.appendChild(imgWrapper)
         }
 
         // Créer une balise <video> pour chaque élément vidéo
@@ -17,8 +38,16 @@ export function createGallery(id, filteredMedia) {
             const video = document.createElement("video");
             video.src = `./assets/gallery/${id}/${item.video}`;
             video.setAttribute('aria-label', `voir la vidéo '${item.title}'`)
-            video.controls = true; // Ajouter des contrôles pour la vidéo
-            grid.appendChild(video);
+            video.controls = true;
+            imgWrapper.appendChild(video);
+            imgTitle.textContent = item.title;
+            likesNumber.textContent = item.likes;
+            likesWrapper.appendChild(likesNumber);
+            likesWrapper.appendChild(likesLogo);
+            imgContent.appendChild(imgTitle);
+            imgContent.appendChild(likesWrapper);
+            imgWrapper.appendChild(imgContent);
+            grid.appendChild(imgWrapper)
         }
     });
 }
