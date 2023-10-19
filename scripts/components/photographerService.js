fc04481b2bce96ec691c6620e6086d68146c94ef
DylanPiriou <dylan.piriou@hotmail.com>
2023-10-19 17:00:27 +0200

    fetch bug test

--- scripts/components/photographerService.js
+++ scripts/components/photographerService.js
@@ -1,7 +1,7 @@
 // Appel des données depuis le JSON
 export async function getPhotographerData() {
     try {
-        const response = await fetch("./../data/photographers.json");
+        const response = await fetch("./data/photographers.json");
         if (!response.ok) {
             throw new Error(`Erreur de chargement du JSON : ${response.statusText}`);
         }
