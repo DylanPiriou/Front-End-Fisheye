fd0785621e722c3ccffd53635651770ef8037d71
DylanPiriou <dylan.piriou@hotmail.com>
2023-11-02 14:39:40 +0100

    feat(photographerCards): infos updated

--- scripts/components/photographerCard.js
+++ scripts/components/photographerCard.js
@@ -1,13 +1,36 @@
 // Création des profils de photographe
 export function createPhotographerCard(photographer) {
-    const { name, portrait } = photographer;
+    console.log(photographer)
+    const { name, portrait, city, country, tagline, price } = photographer;
     const picture = `assets/photographers/${portrait}`;
     const article = document.createElement('article');
     const img = document.createElement('img');
     img.setAttribute("src", picture);
-    const h2 = document.createElement('h2');
-    h2.textContent = name;
+
+    const content = document.createElement('div');
+    content.classList = "photographer_content";
+
+    const photographerName = document.createElement('h2');
+    photographerName.classList = "photographer_name";
+    photographerName.textContent = name;
+    
+    const place = document.createElement('p');
+    place.classList = "photographer_place"
+    place.textContent = `${city}, ${country}`;
+
+    const tag = document.createElement('p');
+    tag.classList = "photographer_tagline";
+    tag.textContent = tagline;
+
+    const pricePerDay = document.createElement('p');
+    pricePerDay.classList = "photographer_price";
+    pricePerDay.textContent = `${price}€/jour`; 
+
     article.appendChild(img);
-    article.appendChild(h2);
+    content.appendChild(photographerName);
+    content.appendChild(place);
+    content.appendChild(tag);
+    content.appendChild(pricePerDay);
+    article.appendChild(content);
     return article;
 }
